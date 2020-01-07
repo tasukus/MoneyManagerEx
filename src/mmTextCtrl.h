db36d32e242314ae9d512f7375624ebb47033ff9
tasuku sasaki <tasukus@gmail.com>
2020-01-08 00:02:28 +0900

    fix 0 to nullptr

--- src/mmTextCtrl.h
+++ src/mmTextCtrl.h
@@ -27,7 +27,7 @@ class mmTextCtrl : public wxTextCtrl
 public:
     using wxTextCtrl::SetValue;
 
-    mmTextCtrl() : m_currency(0) {}
+    mmTextCtrl() : m_currency(nullptr) {}
     mmTextCtrl(wxWindow *parent, wxWindowID id
         , const wxString &value = wxEmptyString
         , const wxPoint &pos = wxDefaultPosition
